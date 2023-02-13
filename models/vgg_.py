5c393d157e83030a874c68872836605591728cc8
ramashish gupta <ramashisx@gmail.com>
2023-02-13 20:39:04 +0530

    init

---
+++ models/vgg_.py
@@ -0,0 +1,218 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+"""
+Mostly copy-paste from torchvision references.
+"""
+from posixpath import split
+import torch
+import torch.nn as nn
+import os
+from tqdm import tqdm
+import urllib.request
+
+
+__all__ = [
+    'VGG', 'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16', 'vgg16_bn',
+    'vgg19_bn', 'vgg19',
+]
+
+
+model_urls = {
+    'vgg11': 'https://download.pytorch.org/models/vgg11-bbd30ac9.pth',
+    'vgg13': 'https://download.pytorch.org/models/vgg13-c768596a.pth',
+    'vgg16': 'https://download.pytorch.org/models/vgg16-397923af.pth',
+    'vgg19': 'https://download.pytorch.org/models/vgg19-dcbb9e9d.pth',
+    'vgg11_bn': 'https://download.pytorch.org/models/vgg11_bn-6002323d.pth',
+    'vgg13_bn': 'https://download.pytorch.org/models/vgg13_bn-abd245e5.pth',
+    'vgg16_bn': 'https://download.pytorch.org/models/vgg16_bn-6c64b313.pth',
+    'vgg19_bn': 'https://download.pytorch.org/models/vgg19_bn-c79401a0.pth',
+}
+
+checkpoints_dir = os.path.join(os.path.dirname(__file__), "../checkpoints/")
+
+model_paths = {
+    'vgg16_bn': os.path.join(checkpoints_dir,'vgg16_bn-6c64b313.pth'),
+    'vgg16': os.path.join(checkpoints_dir,'vgg16-397923af.pth'),
+}
+
+class VGG(nn.Module):
+
+    def __init__(self, features, num_classes=1000, init_weights=True):
+        super(VGG, self).__init__()
+        self.features = features
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, num_classes),
+        )
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+
+def make_layers(cfg, batch_norm=False, sync=False):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                if sync:
+                    print('use sync backbone')
+                    layers += [conv2d, nn.SyncBatchNorm(v), nn.ReLU(inplace=True)]
+                else:
+                    layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+
+cfgs = {
+    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+class DownloadProgressBar(tqdm):
+    def update_to(self, b=1, bsize=1, tsize=None):
+        if tsize is not None:
+            self.total = tsize
+        self.update(b * bsize - self.n)
+
+def download_arch_if_not_exists(arch):
+    if not os.path.exists(model_paths[arch]):
+        print(f"{arch} model not found.")
+        chunk_size = 1024
+        os.makedirs(checkpoints_dir, exist_ok=True)
+
+        url = model_urls[arch]
+        filename = os.path.basename(url)
+        output_path = os.path.join(checkpoints_dir, filename)
+        with DownloadProgressBar(unit='B', unit_scale=True, miniters=1, desc=url.split('/')[-1]) as t:
+            urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
+
+def _vgg(arch, cfg, batch_norm, pretrained, progress, sync=False, **kwargs):
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGG(make_layers(cfgs[cfg], batch_norm=batch_norm, sync=sync), **kwargs)
+    if pretrained:
+        download_arch_if_not_exists(arch)
+        state_dict = torch.load(model_paths[arch])
+        model.load_state_dict(state_dict)
+    return model
+
+
+def vgg11(pretrained=False, progress=True, **kwargs):
+    r"""VGG 11-layer model (configuration "A") from
+    `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _vgg('vgg11', 'A', False, pretrained, progress, **kwargs)
+
+
+def vgg11_bn(pretrained=False, progress=True, **kwargs):
+    r"""VGG 11-layer model (configuration "A") with batch normalization
+    `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _vgg('vgg11_bn', 'A', True, pretrained, progress, **kwargs)
+
+
+def vgg13(pretrained=False, progress=True, **kwargs):
+    r"""VGG 13-layer model (configuration "B")
+    `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _vgg('vgg13', 'B', False, pretrained, progress, **kwargs)
+
+
+def vgg13_bn(pretrained=False, progress=True, **kwargs):
+    r"""VGG 13-layer model (configuration "B") with batch normalization
+    `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _vgg('vgg13_bn', 'B', True, pretrained, progress, **kwargs)
+
+
+def vgg16(pretrained=False, progress=True, **kwargs):
+    r"""VGG 16-layer model (configuration "D")
+    `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _vgg('vgg16', 'D', False, pretrained, progress, **kwargs)
+
+
+def vgg16_bn(pretrained=False, progress=True, sync=False, **kwargs):
+    r"""VGG 16-layer model (configuration "D") with batch normalization
+    `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _vgg('vgg16_bn', 'D', True, pretrained, progress, sync=sync, **kwargs)
+
+
+def vgg19(pretrained=False, progress=True, **kwargs):
+    r"""VGG 19-layer model (configuration "E")
+    `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _vgg('vgg19', 'E', False, pretrained, progress, **kwargs)
+
+
+def vgg19_bn(pretrained=False, progress=True, **kwargs):
+    r"""VGG 19-layer model (configuration 'E') with batch normalization
+    `"Very Deep Convolutional Networks For Large-Scale Image Recognition" <https://arxiv.org/pdf/1409.1556.pdf>`_
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _vgg('vgg19_bn', 'E', True, pretrained, progress, **kwargs)
---
+++ streamlit_demo.py
@@ -0,0 +1,197 @@
+import streamlit as st
+import moviepy.editor as moviepy
+from PIL import Image
+import cv2
+from engine import *
+from models import build_model
+
+
+def object_detection_video(model, device, transform):
+
+    st.title("Crowd Detection for Videos")
+    st.subheader("""
+    
+    This is blah blah blah
+    
+    """)
+    uploaded_video = st.file_uploader("Upload Video", type=['mp4', 'mpeg', 'mov'])
+    if uploaded_video is not None:
+
+        vid = uploaded_video.name
+        with open(vid, mode='wb') as f:
+            f.write(uploaded_video.read())  # save video to disk
+
+        st_video = open(vid, 'rb')
+        video_bytes = st_video.read()
+        st.video(video_bytes)
+        st.write("Uploaded Video")
+
+        cap = cv2.VideoCapture(vid)
+        _, image = cap.read()
+        h, w = image.shape[:2]
+
+        fourcc = cv2.VideoWriter_fourcc(*'mpv4')
+        out = cv2.VideoWriter("detected_video.mp4", fourcc, 20.0, (w, h))
+
+        while True:
+            _, image = cap.read()
+            if _:
+
+                # round the size
+                width, height, _ = image.shape
+                print(width, height)
+                new_width = width // 128 * 32
+                new_height = height // 128 * 32
+                image = cv2.resize(image, (new_height, new_width))
+                # pre-processing
+                img = transform(image)
+
+                print(img.shape)
+
+                samples = torch.Tensor(img).unsqueeze(0)
+                samples = samples.to(device)
+                # run inference
+                outputs = model(samples)
+                outputs_scores = torch.nn.functional.softmax(outputs['pred_logits'], -1)[:, :, 1][0]
+
+                outputs_points = outputs['pred_points'][0]
+
+                threshold = 0.5
+                # filter the predictions
+                points = outputs_points[outputs_scores > threshold].detach().cpu().numpy().tolist()
+                predict_cnt = int((outputs_scores > threshold).sum())
+
+                print(predict_cnt)
+                # draw the predictions
+                size = 2
+                img_to_draw = np.array(image)
+                for p in points:
+                    img_to_draw = cv2.circle(img_to_draw, (int(p[0]), int(p[1])), size, (0, 0, 255), -1)
+
+                out.write(img_to_draw)
+                # cv2.imshow("image", image)
+                #
+                # if ord("q") == cv2.waitKey(1):
+                #     break
+            else:
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+def object_detection_image(model, device, transform):
+    st.title('Crowd Detection for Images')
+    st.subheader("""
+    This is blah blah blah
+    """)
+    file = st.file_uploader('Upload Image', type=['jpg', 'png', 'jpeg'])
+    if file is not None:
+
+        img_raw = Image.open(file).convert('RGB')
+
+        st.image(img_raw, caption="Uploaded Image")
+        my_bar = st.progress(0)
+
+        # round the size
+        width, height = img_raw.size
+        print(width, height)
+        new_width = width // 128 * 128
+        new_height = height // 128 * 128
+        img_raw = img_raw.resize((new_width, new_height), Image.ANTIALIAS)
+        # pre-processing
+        img = transform(img_raw)
+
+        print(img.shape)
+
+        samples = torch.Tensor(img).unsqueeze(0)
+        samples = samples.to(device)
+        # run inference
+        outputs = model(samples)
+        outputs_scores = torch.nn.functional.softmax(outputs['pred_logits'], -1)[:, :, 1][0]
+
+        outputs_points = outputs['pred_points'][0]
+
+        threshold = 0.5
+        # filter the predictions
+        points = outputs_points[outputs_scores > threshold].detach().cpu().numpy().tolist()
+        predict_cnt = int((outputs_scores > threshold).sum())
+
+        print(predict_cnt)
+        # draw the predictions
+        size = 2
+        img_to_draw = np.array(img_raw)
+        for p in points:
+            img_to_draw = cv2.circle(img_to_draw, (int(p[0]), int(p[1])), size, (0, 0, 255), -1)
+
+        st.image(img_to_draw, caption='Processed Image.')
+        my_bar.progress(100)
+
+
+def main():
+    new_title = '<p style="font-size: 42px;">Crowd Detection!</p>'
+    read_me_0 = st.markdown(new_title, unsafe_allow_html=True)
+
+    args = {
+        "backbone": "vgg16_bn",
+        "row": 2,
+        "line": 2,
+        "output_dir": "./logs/",
+        "weight_path": "./weights/SHTechA.pth",
+        "gpu_id": 0
+    }
+
+    device = torch.device('cuda')
+
+    # get the P2PNet
+    model = build_model(args)
+
+    # move to GPU
+    model.to(device)
+
+    # load trained model
+    if args["weight_path"] is not None:
+        checkpoint = torch.load(args["weight_path"], map_location=device)
+        model.load_state_dict(checkpoint['model'])
+
+    # convert to eval mode
+    model.eval()
+
+    # create the pre-processing transform
+    transform = standard_transforms.Compose([
+        standard_transforms.ToTensor(),
+        standard_transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    read_me = st.markdown("""
+    
+    Blah Blah Blah
+    
+    """)
+    st.sidebar.title("Select Activity")
+    choice = st.sidebar.selectbox("MODE", ("About", "Object Detection(Image)", "Object Detection(Video)"))
+
+    if choice == "Object Detection(Image)":
+        read_me_0.empty()
+        read_me.empty()
+        object_detection_image(model, device, transform)
+    elif choice == "Object Detection(Video)":
+        read_me_0.empty()
+        read_me.empty()
+        object_detection_video(model, device, transform)
+        try:
+            clip = moviepy.VideoFileClip('detected_video.mp4')
+            clip.write_videofile("myvideo.mp4")
+            st_video = open('myvideo.mp4', 'rb')
+            video_bytes = st_video.read()
+            st.video(video_bytes)
+            st.write("Detected Video")
+        except OSError:
+            ''
+
+    elif choice == "About":
+        print()
+
+
+if __name__ == '__main__':
+    main()
